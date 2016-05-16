32aaf6835430e61da447876640273cd437a33471
André Bires <andrebires@gmail.com>
2016-05-16 13:10:30 -0300

    Fixed white spaces

--- src/lime/protocol/network/Transport.ts
+++ src/lime/protocol/network/Transport.ts
@@ -3,18 +3,14 @@ import {Envelope} from "../Envelope";
 export interface Transport extends TransportStateListener {
   send(envelope: Envelope): void;
   onEnvelope: (envelope: Envelope) => void;
-
   open(uri: string): void;
   close(): void;
-
   getSupportedCompression(): string[];
   setCompression(compression: string): void;
   compression: string;
-
   getSupportedEncryption(): string[];
   setEncryption(encryption: string): void;
-  encryption: string;
-  
+  encryption: string;  
   isConnected(): boolean;
 }
 
